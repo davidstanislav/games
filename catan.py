49bf42f693cedc09681aaa44f7c42fd682d03c60
Stanislav <David.Stanislav@worthingtonindustries.com>
2022-10-20 12:58:03 -0400

    adds verticies convention

--- catan.py
+++ catan.py
@@ -15,6 +15,8 @@ def create_board():
     # board = [A, B, C, D, E, F, G]
     board = {"A": {}, "B": {}, "C": {}, "D": {}, "E": {}, "F": {}, "G": {}}
     
+
+    
     for i in board.keys():
         j = random.randint(0,len(resource_tiles) - 1)
         board[i]['resource'] = resource_tiles[j]
@@ -29,18 +31,27 @@ def create_board():
         m = random.randint(0,len(number_tiles) - 1)
         board[k]['number'] = number_tiles[m]
         del number_tiles[m]
+    
+    board["A"]["neighborhood"] = {"UR": board["B"], "R": board["C"], "LR": board["D"], "LL": board["E"], "L": board["F"], "UL": board["G"]}
+    board["B"]["neighborhood"] = {"UR": None, "R": None, "LR": board["C"], "LL": board["A"], "L": board["G"], "UL": None}
+    board["C"]["neighborhood"] = {"UR": None, "R": None, "LR": None, "LL": board["D"], "L": board["A"], "UL": board["B"]}
+    board["D"]["neighborhood"] = {"UR": board["C"], "R": None, "LR": None, "LL": None, "L": board["E"], "UL": board["A"]}
+    board["E"]["neighborhood"] = {"UR": board["A"], "R": None, "LR": None, "LL": board["D"], "L": board["A"], "UL": board["F"]}
+    board["F"]["neighborhood"] = {"UR": board["G"], "R": board["A"], "LR": board["E"], "LL": None, "L": None, "UL": None}
+    board["G"]["neighborhood"] = {"UR": None, "R": board["B"], "LR": board["A"], "LL": board["F"], "L": None, "UL": None}
+    
     return(board)
 
 board = create_board()
-
-board["A"]["neighborhood"] = {"UR": board["B"], "R": board["C"], "LR": board["D"], "LL": board["E"], "L": board["F"], "UL": board["G"]}
-board["B"]["neighborhood"] = {"UR": None, "R": None, "LR": board["C"], "LL": board["A"], "L": board["G"], "UL": None}
-board["C"]["neighborhood"] = {"UR": None, "R": None, "LR": None, "LL": board["D"], "L": board["A"], "UL": board["B"]}
-board["D"]["neighborhood"] = {"UR": board["C"], "R": None, "LR": None, "LL": None, "L": board["E"], "UL": board["A"]}
-board["E"]["neighborhood"] = {"UR": board["A"], "R": None, "LR": None, "LL": board["D"], "L": board["A"], "UL": board["F"]}
-board["F"]["neighborhood"] = {"UR": board["G"], "R": board["A"], "LR": board["E"], "LL": None, "L": None, "UL": None}
-board["G"]["neighborhood"] = {"UR": None, "R": board["B"], "LR": board["A"], "LL": board["F"], "L": None, "UL": None}
     
+
+A_verticies = {"1": [board["A"], board["A"]["neighborhood"]['UR'], [board["A"]["neighborhood"]['R']]],
+               "2": [board["A"], board["A"]["neighborhood"]['R'], [board["A"]["neighborhood"]['LR']]],
+               "3": [board["A"], board["A"]["neighborhood"]['LR'], [board["A"]["neighborhood"]['LL']]],
+               "4": [board["A"], board["A"]["neighborhood"]['LL'], [board["A"]["neighborhood"]['L']]],
+               "5": [board["A"], board["A"]["neighborhood"]['L'], [board["A"]["neighborhood"]['UL']]],
+               "6": [board["A"], board["A"]["neighborhood"]['UL'], [board["A"]["neighborhood"]['UR']]],}
+
 """
 The board is now set. Lets create a dictionary to keep track of which resources
 are abundant.
@@ -68,7 +79,7 @@ def roll():
 """
 Simulate game
 """
-for i in range(0, 50):
+for i in range(0, 100):
     roll_i = roll()
     print(roll_i)
     for j in resource_record.keys():
